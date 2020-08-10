3dc8cfaa02234bc7e6b77107c73ede636fc600ea
Sayantan Das <sayantanapril99@gmail.com>
2020-08-10 15:26:13 +0530

    added display name

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
@@ -9,19 +9,32 @@ socketio = SocketIO(app)
 
 _users_in_room = {} # stores room wise user list
 _room_of_sid = {} # stores room joined by an used
+_name_of_sid = {} # stores display name of users
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         room_id = request.form['room_id']
-        return redirect(url_for("enter_room", room_id=room_id))
+        return redirect(url_for("room_landing", room_id=room_id))
 
     return render_template("home.html")
 
 @app.route("/room/<string:room_id>/")
 def enter_room(room_id):
-    return render_template("chatroom.html", room_id=room_id)
+    if room_id not in session:
+        return redirect(url_for("room_landing", room_id=room_id))
+    return render_template("chatroom.html", room_id=room_id, display_name=session[room_id]["name"])
+
+@app.route("/room/<string:room_id>/landing/", methods=["GET", "POST"])
+def room_landing(room_id):
+    if request.method == "POST":
+        display_name = request.form['display_name']
+        session[room_id] = {"name": display_name}
+        return redirect(url_for("enter_room", room_id=room_id))
+
+    return render_template("chatroom_landing.html", room_id=room_id)
+    
 
 
 @socketio.on("connect")
@@ -34,21 +47,24 @@ def on_connect():
 def on_join_room(data):
     sid = request.sid
     room_id = data["room_id"]
+    display_name = session[room_id]["name"]
     
     # register sid to the room
     join_room(room_id)
     _room_of_sid[sid] = room_id
+    _name_of_sid[sid] = display_name
     
     # broadcast to others in the room
-    print("["+ room_id +"] New socket joined ", sid)
-    emit("user-connect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
+    print("[{}] New memeber joined: {}<{}>".format(room_id, display_name, sid))
+    emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
     
     # add to user list maintained on server
     if room_id not in _users_in_room:
         _users_in_room[room_id] = [sid]
         emit("user-list", {"my_id": sid}) # send own id only
     else:
-        emit("user-list", {"list": _users_in_room[room_id], "my_id": sid}) # send list of existing users to the new member
+        usrlist = {u_id:_name_of_sid[u_id] for u_id in _users_in_room[room_id]}
+        emit("user-list", {"list": usrlist, "my_id": sid}) # send list of existing users to the new member
         _users_in_room[room_id].append(sid) # add new member to user list maintained on server
 
     print("\nusers: ", _users_in_room, "\n")
@@ -58,13 +74,17 @@ def on_join_room(data):
 def on_disconnect():
     sid = request.sid
     room_id = _room_of_sid[sid]
+    display_name = _name_of_sid[sid]
 
-    print("["+ room_id +"] Socket disconnected ", sid)
+    print("[{}] Memeber left: {}<{}>".format(room_id, display_name, sid))
     emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
 
     _users_in_room[room_id].remove(sid)
     if len(_users_in_room[room_id]) == 0:
         _users_in_room.pop(room_id)
+
+    _room_of_sid.pop(sid)
+    _name_of_sid.pop(sid)
 
     print("\nusers: ", _users_in_room, "\n")
 
